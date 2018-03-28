154abce3d25dc6feea3d32dc4f04a884fff0f861
Ruben Alliet <allietruben@gmail.com>
2018-03-28 23:58:51 +0200

    material ui

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import {gql, graphql} from 'react-apollo';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Grid from 'material-ui/Grid';
+
 
 const query = gql `{
     books {
@@ -19,13 +21,25 @@ class HomeView extends React.Component {
         return (
             <div>
                 <Header/>
-                {data.books.map((item) => (
+                <Grid container >
+                    <Grid item sm>
+                        Books:
+                        {data.books.map((item) => (
                         <p key={item.id}>
                             <Link to={`/books/${item.id}/`}>
                                 {item.name}
                             </Link>
                         </p>
-                    ))}
+                        ))}
+                    </Grid>
+                    <Grid item sm>
+                        right pane
+                    </Grid>
+                    <Grid item sm>
+                        right pane
+                    </Grid>
+                </Grid>
+                
                 <Footer/>
             </div>
         )
