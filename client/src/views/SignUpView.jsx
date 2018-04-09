91f65a7f82895da5ecfcc3e6e38b147633b929ed
Ruben Alliet <allietruben@gmail.com>
2018-04-10 00:55:48 +0200

    signup view

--- client/src/views/SignUpView.jsx
+++ client/src/views/SignUpView.jsx
@@ -6,8 +6,10 @@ import {gql, graphql} from 'react-apollo';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
 
+//Semantic UI imports
+import { Button, Checkbox, Form, Grid } from 'semantic-ui-react'
+import { Header as HeaderTitle } from 'semantic-ui-react'
 
-//Graphql query imports
 
 class SignUpView extends React.Component {
     render() {
@@ -20,10 +22,34 @@ class SignUpView extends React.Component {
         return (
             <div>
                 <Header/>
-
-                <Link to='/'>Back to homePage</Link>
-
-                <Footer/>
+                <Grid centered columns={2}>
+                    <Grid.Column>
+                        <HeaderTitle as='h1'>Sign up</HeaderTitle>
+                        <Form>
+                            <Form.Field>
+                                <label>Email</label>
+                                <input placeholder='Email' type='email' required />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Name</label>
+                                <input placeholder='Name' type='text' required />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Password</label>
+                                <input placeholder='Password' type='password' required />
+                            </Form.Field>
+                            <Form.Field>
+                                <label>Repeat Password</label>
+                                <input placeholder='Password' type='password' required />
+                            </Form.Field>
+                            <Form.Field>
+                                <Checkbox label='I agree to the Terms and Conditions'/>
+                            </Form.Field>
+                            <Button type='submit'>Register</Button>
+                        </Form>
+                        <Link to='/'>Back to homePage</Link>
+                    </Grid.Column>
+                </Grid>
             </div>
         )
     }
